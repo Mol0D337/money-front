31886c0fed9b4fcab92e27347453ecfd47afc223
Mol0D337 <zmolodyk@gmail.com>
2021-04-24 17:37:56 +0300

    first commit

---
+++ src/views/Auth.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <vue-telegram-login
+            mode="callback"
+            telegram-login="manager_money_bot"
+            @callback="yourCallbackFunction"
+    />
+  </div>
+</template>
+
+<script>
+  import {vueTelegramLogin} from 'vue-telegram-login'
+  export default {
+    name: "Auth",
+    components: {vueTelegramLogin},
+    methods: {
+      yourCallbackFunction (user) {
+        // gets user as an input
+        // id, first_name, last_name, username,
+        // photo_url, auth_date and hash
+        console.log(user)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
